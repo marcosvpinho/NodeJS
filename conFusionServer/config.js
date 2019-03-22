11428f08a37d7d38436c34a322d839b66b99b9f3
Marcos Pinho <marcos.v.pinho@gmail.com>
2019-03-21 23:20:49 -0300

    remove config.js

--- conFusionServer/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
-    'facebook':{
-    	clientId: '2217563294956062',
-    	clientSecret: '6af0ca2b44c3fd78ce8aea69cff5d66b'
-    }
-}
